9efd0d223d20db75eac7e3be2101239f069942fa
Scott Newcomer <snewcomer@users.noreply.github.com>
2021-10-31 21:17:05 -0500

    Export items from base path (#14)

---
+++ addon/index.js
@@ -0,0 +1,5 @@
+import { timeout } from 'ember-stateful-promise/utils/timeout';
+import { StatefulPromise } from 'ember-stateful-promise/utils/stateful-promise';
+import { statefulFunction } from 'ember-stateful-promise/decorators/stateful-function';
+
+export { timeout, StatefulPromise, statefulFunction };
